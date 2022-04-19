2c9ee495121f6cf949db55de3ae8321ccfe26d70
fjyumul <francis_jay_yumul@manulife.ca>
2022-04-19 04:15:02 -0400

    Added minification, sourcemaps, mock api, etc

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -7,14 +7,14 @@
 
 /* eslint-disable no-console */
 
-import { generate, extend } from 'json-schema-faker';
+import jsf from 'json-schema-faker';
 import { schema } from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
 
 // Extend JSF with the fake libs you want to use
-extend('faker', () => require('faker'));
-const json = JSON.stringify(generate(schema));
+jsf.extend('faker', () => require('faker'));
+const json = JSON.stringify(jsf.generate(schema));
 
 fs.writeFile('./src/api/db.json',json,(err) =>{
   if(err) {
@@ -22,4 +22,4 @@ fs.writeFile('./src/api/db.json',json,(err) =>{
   } else {
     console.log(chalk.green("Mock data generated."));
   }
-})
+});
